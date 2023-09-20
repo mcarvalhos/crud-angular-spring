c6caccf7729fbbbb19d35fc8608dec18fe287782
Murilo Carvalho <56516071+mcarvalhos@users.noreply.github.com>
2023-09-20 20:34:20 -0300

    09 - Chamada HTTP Get no Angular e RXJS

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,22 +1,21 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
-@Component({ // Esse @ (DECORATOR) serve pra indicar que é um componente.
+@Component({
+  // Esse @ (DECORATOR) serve pra indicar que é um componente.
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-  courses: Course [] = []; // essa variável PRECISA ser inicializada;
+  courses: Observable<Course[]>; // essa variável PRECISA ser inicializada;
   displayedColumns = ['name', 'category'];
   constructor(private coursesService: CoursesService) {
-   // this.courses =[];  Outra maneira da variável sendo inicializada.
-   // this.courses = this.coursesService.list(); pode ser usado tanto aqui, quanto no "ngOnInit()"
+    // this.courses =[];  Outra maneira da variável sendo inicializada.
+    this.courses = this.coursesService.list(); // pode ser usado tanto aqui, quanto no "ngOnInit()"
   }
 
-  ngOnInit(): void{
-    this.courses = this.coursesService.list();
-  }
+  ngOnInit(): void {}
 }
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from './../model/course';
+import { tap } from 'rxjs';
 
 @Injectable({ // Esse @ (DECORATOR) serve pra indicar que vai fazer parte de injeção de dependências das classes dentro do Angular.
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] { // Esse "Course[]" é opcional e serve pra mostra de forma mais simples que está retornando uma lista de cursos;
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-end' }
-    ];
+  list() { // Esse "Course[]" é opcional e serve pra mostra de forma mais simples que está retornando uma lista de cursos;
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses)) // essa variável "courses" pode ter qualquer nome.
+    );
   }
 }
