f46aa8139a72a5137de0d156ec9f19eb9ad3de7b
Murilo Carvalho <56516071+mcarvalhos@users.noreply.github.com>
2023-09-21 18:06:10 -0300

    11 - Lista de Cursos: Tratamento de Erros e MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   // Esse @ (DECORATOR) serve pra indicar que é um componente.
@@ -12,9 +15,24 @@ import { Observable } from 'rxjs';
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>; // essa variável PRECISA ser inicializada;
   displayedColumns = ['name', 'category'];
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses =[];  Outra maneira da variável sendo inicializada.
-    this.courses$ = this.coursesService.list(); // pode ser usado tanto aqui, quanto no "ngOnInit()"
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
+    ); // pode ser usado tanto aqui, quanto no "ngOnInit()"
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {}
