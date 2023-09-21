a59210005b0e3559f6ded13ec4252ec03089d8e7
Murilo Carvalho <56516071+mcarvalhos@users.noreply.github.com>
2023-09-21 20:48:25 -0300

    15: Listar Cursos (API HTTP GET)

---
+++ crud-spring/src/main/java/com/mcarvalhos/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.mcarvalhos.crudspring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mcarvalhos.crudspring.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
